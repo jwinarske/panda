83bbadc98e0e0fb8aae5beaf341ed57dc6842530
Joel Winarske <joel.winarske@woven-planet.global>
2022-06-03 16:17:10 -0700

    adds signing
    Signed-off-by: Joel Winarske <joel.winarske@woven-planet.global>

--- cmake/gcc-toolchain.cmake
+++ cmake/gcc-toolchain.cmake
@@ -1,9 +1,9 @@
 include_guard()
 
 IF(NOT TRIPLE)
-    set(GCC_TOOLCHAIN_PREFIX "")
+    set(TOOL_PREFIX "")
 else()
-    set(GCC_TOOLCHAIN_PREFIX ${TRIPLE}-)
+    set(TOOL_PREFIX ${TRIPLE}-)
 endif()
 
 message(STATUS "Triple ................. ${TRIPLE}")
@@ -21,7 +21,7 @@ elseif(UNIX OR APPLE)
 endif()
 
 execute_process(
-  COMMAND ${UTIL_SEARCH_CMD} ${GCC_TOOLCHAIN_PREFIX}g++
+  COMMAND ${UTIL_SEARCH_CMD} ${TOOL_PREFIX}gcc
   OUTPUT_VARIABLE BINUTILS_PATH
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
@@ -36,66 +36,49 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-set(CMAKE_AR ${GCC_TOOLCHAIN_PREFIX}ar)
-set(CMAKE_ASM_COMPILER ${GCC_TOOLCHAIN_PREFIX}gcc)
-set(CMAKE_C_COMPILER ${GCC_TOOLCHAIN_PREFIX}gcc)
-
-if("${GCC_TOOLCHAIN_PREFIX}" STREQUAL "x86_64-w64-mingw32" AND "${TARGET}" STREQUAL "host")
-    set(CMAKE_CXX_COMPILER ${GCC_TOOLCHAIN_PREFIX}g++-posix)
-else()
-    set(CMAKE_CXX_COMPILER ${GCC_TOOLCHAIN_PREFIX}g++)
-endif()
+set(CMAKE_AR ${TOOL_PREFIX}ar)
+set(CMAKE_ASM_COMPILER ${TOOL_PREFIX}gcc)
+set(CMAKE_C_COMPILER ${TOOL_PREFIX}gcc)
 
 
-set(CPPFILT ${GCC_TOOLCHAIN_PREFIX}c++filt)
-set(NM ${GCC_TOOLCHAIN_PREFIX}nm)
-set(OBJDUMP ${GCC_TOOLCHAIN_PREFIX}objdump)
-set(OBJCOPY ${GCC_TOOLCHAIN_PREFIX}objcopy)
-set(READELF ${GCC_TOOLCHAIN_PREFIX}readelf)
-set(SIZE ${GCC_TOOLCHAIN_PREFIX}size)
+set(CMAKE_CPPFILT ${TOOL_PREFIX}c++filt)
+set(CMAKE_NM ${TOOL_PREFIX}nm)
+set(CMAKE_OBJDUMP ${TOOL_PREFIX}objdump)
+set(CMAKE_OBJCOPY ${TOOL_PREFIX}objcopy)
+set(CMAKE_READELF ${TOOL_PREFIX}readelf)
+set(CMAKE_SIZE ${TOOL_PREFIX}size)
 set(CMAKE_EXECUTABLE_SUFFIX .elf)
 
 set(GCCFLAGS
-#    -g
-#    -Wall
-#    -Wextra
-#    -pedantic
-#    -Wmain
-#    -Wundef
-#    -Wsign-conversion
-#    -Wunused-parameter
-#    -Wuninitialized
-#    -Wmissing-declarations
-#    -Wshadow
-#    -Wunreachable-code
-#    -Wswitch-default
-#    -Wswitch-enum
-#    -Wcast-align
-#    -Wmissing-include-dirs
-#    -Winit-self
-#    -Wfloat-equal
-#    -Wdouble-promotion
-#    -Wno-comment
-#    -gdwarf-2
-#    -fno-exceptions
-#    -ffunction-sections
-#    -fdata-sections
+    -g
+    -Wall
+    -Wextra
+    -pedantic
+    -Wmain
+    -Wundef
+    -Wsign-conversion
+    -Wunused-parameter
+    -Wuninitialized
+    -Wmissing-declarations
+    -Wshadow
+    -Wunreachable-code
+    -Wswitch-default
+    -Wswitch-enum
+    -Wcast-align
+    -Wmissing-include-dirs
+    -Winit-self
+    -Wfloat-equal
+    -Wdouble-promotion
+    -Wno-comment
+    -gdwarf-2
+    -fno-exceptions
+    -ffunction-sections
+    -fdata-sections
 )
 
 set(_CFLAGS ${GCCFLAGS}
-#    -Wunsuffixed-float-constants
+    -Wunsuffixed-float-constants
     -x c
-)
-
-set(_CXXFLAGS ${GCCFLAGS}
-    -x c++
-    -fno-rtti
-    -fno-use-cxa-atexit
-    -fno-nonansi-builtins
-    -fno-threadsafe-statics
-    -fno-enforce-eh-specs
-    -ftemplate-depth=32
-    -Wzero-as-null-pointer-constant
 )
 
 set(_AFLAGS ${GCCFLAGS}
@@ -105,13 +88,11 @@ set(_AFLAGS ${GCCFLAGS}
 set(_LDFLAGS ${GCCFLAGS}
     -x none
     -Wl,--gc-sections
-#    -Wl,-Map,${APP}.map
 )
 
 set(PARSE_SYMBOL_OPTIONS --print-size)
 
 # remove list item delimeter
 string(REPLACE ";" " " CMAKE_C_FLAGS "${_CFLAGS}")
-string(REPLACE ";" " " CMAKE_CXX_FLAGS "${_CXXFLAGS}")
 string(REPLACE ";" " " CMAKE_ASM_FLAGS "${_AFLAGS}")
 string(REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS "${_LDFLAGS}")
